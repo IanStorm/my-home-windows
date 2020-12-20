4b007fe14ee9b983377b09b2645c45be000dfa56
Christian Sporer <IanStorm28@outlook.com>
2020-12-20 16:34:40 +0100

    Add first setup script

---
+++ odysseus.ps1
@@ -0,0 +1,19 @@
+winget install Atlassian.Sourcetree -v 3.3.8
+winget install Discord.Discord
+winget install Docker.DockerDesktop
+winget install Dropbox.Dropbox
+winget install Google.BackupAndSync
+winget install Logitech.LGS
+winget install Microsoft.OneDrive
+winget install Microsoft.Powertoys
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.Whiteboard -s msstore
+winget install Microsoft.WindowsTerminal -s msstore
+winget install Mozilla.Firefox
+winget install NickeManarin.ScreenToGif
+winget install Nvidia.GeForceExperience
+winget install RaspberryPiFoundation.RaspberryPiImager
+winget install TeamViewer.TeamViewer
+winget install RARLab.WinRAR
+winget install Valve.Steam
+winget install WiresharkFoundation.Wireshark
