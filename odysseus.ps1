4a506329f9bd4d3740f79d03083416c558aadf2c
Christian Sporer <IanStorm28@outlook.com>
2021-05-23 09:27:44 +0200

    Add GitHub Desktop

--- odysseus.ps1
+++ odysseus.ps1
@@ -1,6 +1,7 @@
 winget install Atlassian.Sourcetree -v 3.3.8
 winget install Discord.Discord
 winget install Docker.DockerDesktop
+winget install GitHub.GitHubDesktop
 winget install Google.BackupAndSync
 winget install Logitech.LGS
 winget install Microsoft.MouseAndKeyboardCenter#FIXME: 404
