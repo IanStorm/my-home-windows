b6b68b307948a713e431645438e8fb1e5327cc67
Christian Sporer <IanStorm28@outlook.com>
2021-06-24 21:49:17 +0200

    Add setup script

---
+++ tree.ps1
@@ -0,0 +1,12 @@
+# Bitwarden -s msstore
+winget install Dropbox.Dropbox
+winget install ElectronicArts.EADesktop
+#TODO: winget install Google.BackupAndSync
+winget install Logitech.SetPoint
+# winget install Microsoft.OneDrive
+winget install Microsoft.VisualStudioCode -i
+winget install NexusMods.Vortex
+winget install Parsec.Parsec
+winget install RARLab.WinRAR
+winget install Ubisoft.Connect
+winget install Valve.Steam
